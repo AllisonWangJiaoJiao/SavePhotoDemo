f7f29b089b3de9c579cc9202cd5e2e655250d680
allison <2807338860@qq.com>
2018-08-13 18:21:31 +0800

    BDFCustomPhotoAlbum

--- SavePhotoDemo/SavePhotoDemo/BDFCustomPhotoAlbum.m
+++ SavePhotoDemo/SavePhotoDemo/BDFCustomPhotoAlbum.m
@@ -1,32 +1,45 @@
 //
-//  ViewController.m
+//  BDFCustomPhotoAlbum.m
 //  SavePhotoDemo
 //
-//  Created by allison on 2018/8/11.
+//  Created by allison on 2018/8/13.
 //  Copyright © 2018年 allison. All rights reserved.
 //
 
-#import "ViewController.h"
-#import <Photos/Photos.h>
 #import "BDFCustomPhotoAlbum.h"
+#import <Photos/Photos.h>
 
-@interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property(strong,nonatomic) UIImage *image;
-/// 当前App对应的自定义相册
-- (PHAssetCollection*)createCollection;
-/// 返回刚才保存到【相机胶卷】的图片
-- (PHFetchResult<PHAsset *> *)createdAssets;
+@interface BDFCustomPhotoAlbum() <NSCopying, NSMutableCopying>
 @end
 
-@implementation ViewController
+@implementation BDFCustomPhotoAlbum
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    self.image = [UIImage imageNamed:@"image2"];
-    self.imageView.image = self.image;
+static BDFCustomPhotoAlbum *_shareInstance;
++ (instancetype)shareInstance {
+    if (_shareInstance == nil) {
+        _shareInstance = [[self alloc] init];
+    }
+    return _shareInstance;
 }
+
++ (instancetype)allocWithZone:(struct _NSZone *)zone {
+    if (!_shareInstance) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            _shareInstance = [super allocWithZone:zone];
+        });
+    }
+    return _shareInstance;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    return _shareInstance;
+}
+
+- (id)mutableCopyWithZone:(NSZone *)zone {
+    return _shareInstance;
+}
+
 #pragma mark -- <获取当前App对应的自定义相册>
 - (PHAssetCollection*)createCollection {
     //获取App名字
@@ -56,34 +69,36 @@
 }
 
 #pragma mark -- <获取相片>
-- (PHFetchResult<PHAsset *> *)createdAssets {
+- (PHFetchResult<PHAsset *> *)createdAssets:(nonnull UIImage *)image {
     // 同步执行修改操作
     NSError *error = nil;
     __block NSString *assertId = nil;
     // 保存图片到【相机胶卷】
     [[PHPhotoLibrary sharedPhotoLibrary]performChangesAndWait:^{
-        assertId =  [PHAssetChangeRequest creationRequestForAssetFromImage:self.imageView.image].placeholderForCreatedAsset.localIdentifier;
+        assertId =  [PHAssetChangeRequest creationRequestForAssetFromImage:image].placeholderForCreatedAsset.localIdentifier;
     } error:&error];
     if (error) {
         NSLog(@"保存失败");
         return nil;
     }
     // 获取相片
-     PHFetchResult<PHAsset *> *createdAssets = [PHAsset fetchAssetsWithLocalIdentifiers:@[assertId] options:nil];
+    PHFetchResult<PHAsset *> *createdAssets = [PHAsset fetchAssetsWithLocalIdentifiers:@[assertId] options:nil];
     return createdAssets;
 }
 
 #pragma mark --  <保存图片到相册>
-- (void)saveimageIntoAlbum {
+- (void)saveimageIntoAlbum:(nonnull UIImage *)image  {
     // 1.先保存图片到【相机胶卷】
-    PHFetchResult<PHAsset *> *createdAssets = self.createdAssets;
+    PHFetchResult<PHAsset *> *createdAssets = [self createdAssets:image];
     if (createdAssets == nil) {
-        NSLog(@"保存图片失败");
+        // NSLog(@"保存图片失败");
+        [self showAlertMessage:@"保存图片失败"];
     }
     // 2.拥有一个【自定义相册】
     PHAssetCollection * assetCollection = self.createCollection;
     if (assetCollection == nil) {
-        NSLog(@"创建相册失败");
+        // NSLog(@"创建相册失败");
+        [self showAlertMessage:@"创建相册失败"];
     }
     // 3.将刚才保存到【相机胶卷】里面的图片引用到【自定义相册】
     NSError *error = nil;
@@ -93,12 +108,16 @@
         [requtes insertAssets:createdAssets atIndexes:[NSIndexSet indexSetWithIndex:0]];
     } error:&error];
     if (error) {
-        NSLog(@"保存图片失败");
+        // NSLog(@"保存图片失败");
+        [self showAlertMessage:@"保存图片失败"];
     } else {
-        NSLog(@"保存图片成功");
+        // NSLog(@"保存图片成功");
+        [self showAlertMessage:@"创建相册失败"];
     }
 }
-- (IBAction)saveToNewThumb:(UIButton *)sender {
+
+- (void)saveToNewThumb:(nonnull UIImage *)image {
+    
     PHAuthorizationStatus oldStatus = [PHPhotoLibrary authorizationStatus];
     // 检查用户访问权限
     // 如果用户还没有做出选择，会自动弹框
@@ -110,48 +129,26 @@
                     NSLog(@"提醒用户打开开关");
                 }
             } else if (status == PHAuthorizationStatusAuthorized) { // 用户允许当前App访问
-                [self saveimageIntoAlbum];
+                [self saveimageIntoAlbum:image];
             } else if (status == PHAuthorizationStatusRestricted) { // 无法访问相册
                 NSLog(@"因系统原因，无法访问相册");
             }
         });
     }];
-
 }
 
-- (IBAction)saveClick:(UIButton *)sender {
-    //参数1:图片对象
-    //参数2:成功方法绑定的target
-    //参数3:成功后调用方法
-    //参数4:需要传递信息(成功后调用方法的参数) 一般写nil
-    UIImageWriteToSavedPhotosAlbum(self.imageView.image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
-    /* 1.先保存图片到【相机胶卷】(不能直接保存到自定义相册中)
-        1> C语言函数
-        2> AssetsLibrary框架  (iOS4支持，iOS9.0被废弃)
-        3> Photos框架 (iOS 8.0支持 推荐使用)
-       2.拥有一个【自定义相册】
-        1> AssetsLibrary框架
-        2> Photos框架
-       3.将刚才保存到【相机胶卷】里面的图片引用到【自定义相册】
-        1> AssetsLibrary框架
-        2> Photos框架
-     */
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+- (void)showAlertMessage:(NSString *)message {
+    UIAlertView *alert = [[UIAlertView alloc]initWithTitle:nil
+                                                   message:message
+                                                  delegate:nil
+                                         cancelButtonTitle:nil
+                                         otherButtonTitles:@"确定", nil];
     
-}
-#pragma mark -- <保存到相册>
--(void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
-    NSString *msg = nil ;
-    if(error){
-        msg = @"保存图片失败" ;
-    }else{
-        msg = @"保存图片成功" ;
-    }
+    [alert show];
 }
 
-#pragma mark -- <直接调用封装的工具类保存图片到相册>
-- (IBAction)shareInstanceSaveToNewThumb:(UIButton *)sender {
-    [[BDFCustomPhotoAlbum shareInstance]saveToNewThumb:self.imageView.image];
-}
 
 
 @end
